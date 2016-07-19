bae08aafee9f1471f4ab78df7d782504d2b281af
libing <libing7569@126.com>
2016-07-19 07:16:48 +0000

    tes

--- bitree/bitree_test.go
+++ bitree/bitree_test.go
@@ -3,7 +3,7 @@ package bitree
 import "fmt"
 import "testing"
 
-func testBiTree(t *testing.T) {
+func TestBiTree(t *testing.T) {
 	bt := New()
 	node := &BiNode{1000, &BiNode{9999999, nil, nil}, nil}
 	bt.root = &BiNode{100, node, &BiNode{2000, &BiNode{99, nil, nil}, nil}}
