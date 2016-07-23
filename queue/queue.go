7a6a049f0cc760f5dcba200c2c53064cbed08ea1
李兵 <libing@libingdeMacBook-Pro.local>
2016-07-23 21:50:47 +0800

    queue

--- queue/queue.go
+++ queue/queue.go
@@ -2,28 +2,26 @@ package queue
 
 import "errors"
 
-type Queue struct {
-	b []interface{}
-}
+type Queue []interface{}
 
-func New(size uint32) *Queue {
-	return &Queue{make([]interface{}, 0, size)}
+func New(size uint32) Queue {
+	return make(Queue, 0, size)
 }
 
 func (q *Queue) En(e interface{}) {
-	q.b = append(q.b, e)
+	*q = append(*q, e)
 }
 
 func (q *Queue) De() (e interface{}, err error) {
-	if len(q.b) == 0 {
+	if len(*q) == 0 {
 		return nil, errors.New("Queue is empty")
 	}
 
-	e, err = q.b[0], nil
-	q.b = q.b[1:]
+	e, err = (*q)[0], nil
+	*q = (*q)[1:]
 	return
 }
 
 func (q *Queue) Clear() {
-	q.b = []interface{}{}
+	*q = []interface{}{}
 }
